155abb8fff7e1e6571f1979b5782f1bbbe1a7e09
Wandonium <hillarywando@gmail.com>
2022-04-30 16:11:00 +0300

    Finished User update. Adding Redux to project.

--- src/components/Posts/UserPosts.tsx
+++ src/components/Posts/UserPosts.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import './UserPosts.css';
+import { Loading } from '../Loading/Loading';
 
 
 type User = {
@@ -34,34 +35,41 @@ interface UserPostsProps {
 
 export const UserPosts: React.FC<UserPostsProps> = ({user}) => {
     const [posts, setPosts] = useState<Post[]>([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
+        setLoading(true);
         const url = `https://jsonplaceholder.typicode.com/posts?userId=${user?.id}`;
         axios.get(url)
             .then(response => {
-                console.log('response: ', response.data);
+                // console.log('response: ', response.data);
                 setPosts(response.data);
+                setLoading(false);
             });
     }, []);
 
     return (
-        <div className="px-5">
-            <h4>Showing all posts from user: <span className="badge bg-secondary" style={{fontSize: '1em'}}>{user?.username}</span></h4>
-            <div className="mt-3">
-                { posts.map(post => {
-                    return (
-                        <div className="card mb-5 bg-success card-p">
-                            <div className="card-header">
-                                {post.title}
-                            </div>
-                            <div className="card-body">
-                                <h5 className="card-title">{post.body}</h5>
-                                <p className="card-text">{`by ${user?.username} (${user?.name})`}</p>
-                            </div>
-                        </div>
-                    )
-                })}
-            </div>
+        <div>
+            { loading ? <Loading /> : (
+                <div className="px-5">
+                    <h4>Showing all posts from user: <span className="badge bg-secondary" style={{fontSize: '1em'}}>{user?.username}</span></h4>
+                    <div className="mt-3">
+                        { posts.map(post => {
+                            return (
+                                <div className="card mb-5 bg-success card-p">
+                                    <div className="card-header">
+                                        {post.title}
+                                    </div>
+                                    <div className="card-body">
+                                        <h5 className="card-title">{post.body}</h5>
+                                        <p className="card-text">{`by ${user?.username} (${user?.name})`}</p>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
--- src/components/Users/UserTable.tsx
+++ src/components/Users/UserTable.tsx
@@ -2,6 +2,9 @@ import React, {FunctionComponent, useEffect, useState} from 'react';
 import axios from "axios";
 import './UserTable.css';
 import { Loading } from '../Loading/Loading';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Modal, Button, Form } from 'react-bootstrap';
+
 
 type User = {
     id: number,
@@ -36,6 +39,36 @@ const UsersTable: FunctionComponent<Props> = ({
 }) => {
     const [users, setUsers] = useState<User[]>([]);
     const [loading, setLoading] = useState(false);
+    const [show, setShow] = useState(false);
+    const [updateUser, setUpdateUser] = useState<User>();
+    const [newName, setNewName] = useState('');
+
+    const handleClose = () => setShow(false);
+    const handleUpdateUser = () => {
+        setShow(false);
+        console.log('newName: ', newName);
+        setLoading(true);
+        axios.put(
+            `https://jsonplaceholder.typicode.com/users/${updateUser?.id}`,
+            {
+                ...updateUser,
+                name: newName,
+            },
+            {
+                headers: {
+                  'Content-type': 'application/json; charset=UTF-8',
+                },
+            }
+        ).then(res => {
+            let updatedUser = res.data;
+            console.log('updatedUser: ', JSON.stringify(updatedUser, null, 2));
+            let theUsers = [...users];
+            const updateIndex = theUsers.findIndex(user => user.id === updatedUser.id);
+            theUsers.splice(updateIndex, 1, updatedUser);
+            setUsers(theUsers);
+            setLoading(false);
+        })
+    }
 
 
     const profileImages = [
@@ -84,49 +117,86 @@ const UsersTable: FunctionComponent<Props> = ({
         <div>
             {
                 loading ? <Loading /> : (
-    
-                    <div className="table-responsive">
-                        <table className="table table-hover table-nowrap">
-                            <thead className="thead-light">
-                                <tr>
-                                    <th scope="col">id</th>
-                                    <th scope="col">name</th>
-                                    <th scope="col">username</th>
-                                    <th scope="col">email</th>
-                                    <th scope="col">phone</th>
-                                    <th scope="col">website</th>
-                                    <th scope="col" className="text-center">actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {users.map(user => <tr key={user.id} className="table-row">
-                                    <td>{user.id}</td>
-                                    <td>
-                                        <img alt="..." src={profileImages[user.id -1]} className="avatar avatar-sm rounded-circle me-2"/>
-                                        <a className="text-heading font-semibold name-link" href="#">{user.name}</a>
-                                    </td>
-                                    <td>{user.username}</td>
-                                    <td>{user.email}</td>
-                                    <td>{user.phone}</td>
-                                    <td>{user.website}</td>
-                                    <td>
-                                        <div className="row">
-                                            <div className="col">
-                                                <button onClick={() => {
-                                                    setCurrentUser(user);
-                                                    setActive('posts');
-                                                    setShowPostNav(true);
-                                                }} className="btn-sm btn-primary mr-3">View Posts</button>
+
+                    <div>
+                        <Modal show={show} onHide={handleClose} centered>
+                            <Modal.Header closeButton>
+                            <Modal.Title>Update {updateUser?.username}'s name</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                            <Form>
+                                <Form.Group className="mb-3">
+                                <Form.Label>Current Name: {updateUser?.name}</Form.Label>
+                                </Form.Group>
+                                <Form.Group className="mb-3">
+                                <Form.Label>New Name:</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    autoFocus
+                                    onChange={e => setNewName(e.target.value)}
+                                />
+                                </Form.Group>
+                            </Form>
+                            </Modal.Body>
+                            <Modal.Footer>
+                            <Button variant="secondary" onClick={handleClose}>
+                                Close
+                            </Button>
+                            <Button variant="primary" onClick={handleUpdateUser}>
+                                Save Changes
+                            </Button>
+                            </Modal.Footer>
+                        </Modal>
+
+                        {/* Used template from https://webpixels.io/components/application/tables/advanced-tables */}
+                        {/* Copied css code into WebPixels.css so as to always have it locally and reduce load time */}
+                        <div className="table-responsive">
+                            <table className="table table-hover table-nowrap">
+                                <thead className="thead-light">
+                                    <tr>
+                                        <th scope="col">id</th>
+                                        <th scope="col">name</th>
+                                        <th scope="col">username</th>
+                                        <th scope="col">email</th>
+                                        <th scope="col">phone</th>
+                                        <th scope="col">website</th>
+                                        <th scope="col" className="text-center">actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {users.map(user => <tr key={user.id} className="table-row">
+                                        <td>{user.id}</td>
+                                        <td>
+                                            <img alt="..." src={profileImages[user.id -1]} className="avatar avatar-sm rounded-circle me-2"/>
+                                            <a className="text-heading font-semibold name-link" href="#">{user.name}</a>
+                                        </td>
+                                        <td>{user.username}</td>
+                                        <td>{user.email}</td>
+                                        <td>{user.phone}</td>
+                                        <td>{user.website}</td>
+                                        <td>
+                                            <div className="row">
+                                                <div className="col">
+                                                    <button onClick={() => {
+                                                        setCurrentUser(user);
+                                                        setActive('posts');
+                                                        setShowPostNav(true);
+                                                    }} className="btn-sm btn-primary mr-3">View Posts</button>
+                                                </div>
+                                                <div className="col">
+                                                    <button onClick={() => {
+                                                        setUpdateUser(user);
+                                                        setShow(true);
+                                                    }} className="btn-sm btn-success">Update Name</button>
+                                                </div>
                                             </div>
-                                            <div className="col">
-                                                <button className="btn-sm btn-success">Update User</button>
-                                            </div>
-                                        </div>
-                                    </td>
-                                </tr>)}
-                            </tbody>
-                        </table>
+                                        </td>
+                                    </tr>)}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
+    
                 )
             }
         </div>
