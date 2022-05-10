3c58ec2cb3d96a48bf0ae778858c05cca414e926
Wandonium <hillarywando@gmail.com>
2022-05-10 09:21:44 +0300

    test deployment

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -30,7 +30,7 @@ export const Home: React.FC<HomeProps> = ({setActive, showPostNav, setShowPostNa
 
     return (
         <div className="px-4 py-5 my-3 text-center">
-            <h1 className="display-5 fw-bold">Savannah Informatics Demo</h1>
+            <h1 className="display-5 fw-bold">Savannah Informatics Demonstration</h1>
             <div className="col-lg-6 mx-auto">
             <p className="lead mb-4">This demo makes use of data from <a className="jp-logo" href="https://jsonplaceholder.typicode.com/">JSON Placeholder</a>. Click on the buttons in the table to either update a particular user's details or view a particular user's posts. Click on the Addresses link to view the number of users living in each address.</p>
             <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
